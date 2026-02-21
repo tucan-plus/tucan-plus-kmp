b833b0523bad0ee5c87a8926fc0564b76f6d82d5
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2026-02-21 21:21:35 +0100

    wip

--- composeApp/src/wasmJsMain/kotlin/de/selfmade4u/tucanpluskmp/Platform.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/de/selfmade4u/tucanpluskmp/Platform.wasmJs.kt
@@ -1,7 +1,21 @@
 package de.selfmade4u.tucanpluskmp
 
+import androidx.compose.ui.platform.UriHandler
+
 class WasmPlatform : Platform {
     override val name: String = "Web with Kotlin/Wasm"
 }
 
 actual fun getPlatform(): Platform = WasmPlatform()
+
+@OptIn(ExperimentalWasmJsInterop::class)
+fun registerProtocolHandler(): Unit = js(
+    """{
+        navigator.registerProtocolHandler("web+dedatenlotsencampusnettuda", "http://localhost:8080/?to=%s");
+}"""
+)
+
+actual fun getLoginUrl(uriHandler: UriHandler): String {
+    registerProtocolHandler()
+    return "Test"
+}
