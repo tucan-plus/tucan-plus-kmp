4c097d5530e06a3243432dfd05baf4b381f5e806
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2026-02-21 21:07:58 +0100

    wip

--- composeApp/src/androidMain/kotlin/de/selfmade4u/tucanpluskmp/Platform.android.kt
+++ composeApp/src/androidMain/kotlin/de/selfmade4u/tucanpluskmp/Platform.android.kt
@@ -1,9 +1,17 @@
 package de.selfmade4u.tucanpluskmp
 
 import android.os.Build
+import androidx.compose.ui.platform.UriHandler
 
 class AndroidPlatform : Platform {
     override val name: String = "Android ${Build.VERSION.SDK_INT}"
 }
 
 actual fun getPlatform(): Platform = AndroidPlatform()
+
+actual fun getLoginUrl(uriHandler: UriHandler): String {
+    val url = "https://dsf.tucan.tu-darmstadt.de/IdentityServer/connect/authorize?client_id=MobileApp&scope=openid+DSF+profile+offline_access&response_mode=query&response_type=code&ui_locales=de&redirect_uri=de.datenlotsen.campusnet.tuda:/oauth2redirect"
+    uriHandler.openUri(url)
+
+    return "Test"
+}
