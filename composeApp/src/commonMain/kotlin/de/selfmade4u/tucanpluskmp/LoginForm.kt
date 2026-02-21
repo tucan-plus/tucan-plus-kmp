c6850da04623440a64de1590d7a94ecbd0279b59
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2026-02-21 22:04:41 +0100

    wip

--- composeApp/src/commonMain/kotlin/de/selfmade4u/tucanpluskmp/LoginForm.kt
+++ composeApp/src/commonMain/kotlin/de/selfmade4u/tucanpluskmp/LoginForm.kt
@@ -1,9 +1,20 @@
 package de.selfmade4u.tucanpluskmp
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
 import androidx.navigation3.runtime.NavBackStack
 import androidx.navigation3.runtime.NavKey
 
@@ -14,6 +25,20 @@ class NavBackStackPreviewParameterProvider : PreviewParameterProvider<NavBackSta
 @Composable
 @Preview
 fun LoginForm(@PreviewParameter(NavBackStackPreviewParameterProvider::class) backStack: NavBackStack<NavKey>) {
-
-
+    val snackbarHostState = remember { SnackbarHostState() }
+    Scaffold(modifier = Modifier.fillMaxSize(), snackbarHost = {
+        SnackbarHost(hostState = snackbarHostState)
+    }) { innerPadding ->
+        Column(
+            Modifier
+                .padding(innerPadding)
+                .padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            //ShowLocalServices()
+            //WifiDirect()
+            //WifiDirectBonjour()
+            Text("Logging in...")
+        }
+    }
 }
