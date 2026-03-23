87b8b74b5463134d572660ddd8683f40e7e5d487
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2026-03-23 23:30:08 +0100

    wip

---
+++ composeApp/src/commonMain/kotlin/de/selfmade4u/tucanpluskmp/connector/MyCoursesConnector.kt
@@ -0,0 +1,5 @@
+package de.selfmade4u.tucanpluskmp.connector
+
+// TODO https://github.com/tucan-plus/tucan-plus-android/blob/2fa-support/connector/src/main/java/de/selfmade4u/tucanplus/connector/MyCourses.kt
+class MyCoursesConnector {
+}
