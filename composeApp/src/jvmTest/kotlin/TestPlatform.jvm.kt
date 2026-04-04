aaddf63a3703afc11a60ff08933b25afb89f153d
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2026-04-04 17:14:02 +0200

    desktop and android tests

--- composeApp/src/jvmTest/kotlin/TestPlatform.jvm.kt
+++ composeApp/src/jvmTest/kotlin/TestPlatform.jvm.kt
@@ -1,9 +1,31 @@
 package de.selfmade4u.tucanpluskmp
 
+import androidx.compose.ui.test.ComposeUiTest
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.runComposeUiTest
 import androidx.room3.Room
 import androidx.sqlite.driver.bundled.BundledSQLiteDriver
+import kotlinx.coroutines.test.TestResult
+import kotlin.coroutines.CoroutineContext
+import kotlin.time.Duration
 
 // https://github.com/androidx/androidx/commit/47342d974c778259bd81f9af65d177d64ffda237
 actual fun getTestDatabase(): AppDatabase {
     return Room.inMemoryDatabaseBuilder<AppDatabase>().setDriver(BundledSQLiteDriver()).build()
+}
+
+@OptIn(ExperimentalTestApi::class)
+actual fun runMyComposeUiTest(
+    effectContext: CoroutineContext,
+    runTestContext: CoroutineContext,
+    testTimeout: Duration,
+    block: suspend ComposeUiTest.() -> Unit,
+): TestResult = runComposeUiTest (effectContext, runTestContext, testTimeout) {
+    initKoin {
+
+    }
+    setContent {
+        App(null)
+    }
+    block()
 }
