5e4f588b56fa2072601b92d60993eef2a095cb65
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2026-04-17 23:50:14 +0200

    Idea

--- composeApp/src/jvmMain/kotlin/de/selfmade4u/tucanpluskmp/connector_generator/ConnectorGeneratorMain.kt
+++ composeApp/src/jvmMain/kotlin/de/selfmade4u/tucanpluskmp/connector_generator/ConnectorGeneratorMain.kt
@@ -1,8 +1,16 @@
 package de.selfmade4u.tucanpluskmp.connector_generator
 
 import com.intellij.lang.ASTNode
+import com.intellij.openapi.extensions.ExtensionPoint
 import com.intellij.openapi.util.Disposer
+import com.intellij.openapi.util.UserDataHolderBase
+import com.intellij.pom.PomModel
+import com.intellij.pom.PomModelAspect
+import com.intellij.pom.PomTransaction
+import com.intellij.pom.impl.PomTransactionBase
+import com.intellij.pom.tree.TreeAspect
 import com.intellij.psi.PsiElement
+import com.intellij.psi.impl.source.tree.TreeCopyHandler
 import org.jetbrains.kotlin.cli.jvm.compiler.IdeaStandaloneExecutionSetup
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreApplicationEnvironment
 import org.jetbrains.kotlin.cli.jvm.compiler.KotlinCoreApplicationEnvironmentMode
@@ -13,6 +21,7 @@ import org.jetbrains.kotlin.psi.KtDeclarationWithBody
 import org.jetbrains.kotlin.psi.KtFile
 import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.KtPsiFactory
+import sun.reflect.ReflectionFactory
 import kotlin.getValue
 
 val project by lazy {
@@ -32,7 +41,36 @@ val project by lazy {
 fun createKtFile(codeString: String, fileName: String) =
     KtPsiFactory(project, markGenerated = false).createFile(fileName, codeString)
 
+
+private class FormatPomModel : UserDataHolderBase(), PomModel {
+    override fun runTransaction(transaction: PomTransaction) {
+        (transaction as PomTransactionBase).run()
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    override fun <T : PomModelAspect> getModelAspect(aspect: Class<T>): T? {
+        if (aspect == TreeAspect::class.java) {
+            // using approach described in https://git.io/vKQTo due to the magical bytecode of TreeAspect
+            // (check constructor signature and compare it to the source)
+            // (org.jetbrains.kotlin:kotlin-compiler-embeddable:1.0.3)
+            val constructor =
+                ReflectionFactory
+                    .getReflectionFactory()
+                    .newConstructorForSerialization(
+                        aspect,
+                        Any::class.java.getDeclaredConstructor(*arrayOfNulls<Class<*>>(0)),
+                    )
+            return constructor.newInstance() as T
+        }
+        return null
+    }
+}
+
 fun main() {
+    val extension = "org.jetbrains.kotlin.com.intellij.treeCopyHandler"
+    val extensionClass = TreeCopyHandler::class.java.name
+    project.registerService(PomModel::class.java, FormatPomModel())
+    val psiFactory = KtPsiFactory(project, markGenerated = false)
     val test = createKtFile("""
         fun main() {
         
@@ -41,7 +79,8 @@ fun main() {
     for (declaration in test.declarations) {
         println(declaration::class)
         if (declaration is KtNamedFunction) {
-            println(declaration.bodyBlockExpression!!.add())
+            declaration.bodyBlockExpression!!.addChild(psiFactory.createExpression("println(\"hello world\")").node)
         }
     }
+    println(test.text)
 }
