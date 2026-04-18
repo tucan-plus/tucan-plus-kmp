268e858154ebf62a0bfbf9fa38fb9a038f9fe75b
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2026-04-19 00:57:42 +0200

    It builds

---
+++ html-extractor/src/main/kotlin/de/selfmade4u/FolderAction.kt
@@ -0,0 +1,16 @@
+package de.selfmade4u
+
+import com.intellij.openapi.actionSystem.ActionUpdateThread
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.project.DumbAwareAction
+
+internal class FolderAction : DumbAwareAction() {
+    override fun getActionUpdateThread(): ActionUpdateThread = ActionUpdateThread.BGT
+
+    override fun update(e: AnActionEvent) {
+    }
+
+    override fun actionPerformed(e: AnActionEvent) {
+
+    }
+}
