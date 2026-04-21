a6b682e0a80db2dd519c47ca29076f136bac97af
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2026-04-21 22:52:10 +0200

    wip

--- intellij-platform-modular-plugin-template/frontend/src/main/kotlin/org/jetbrains/plugins/template/ModularPluginIcons.kt
+++
@@ -1,9 +0,0 @@
-package org.jetbrains.plugins.template
-
-import com.intellij.ui.IconManager
-
-@Suppress("unused")
-object ModularPluginIcons {
-    @JvmField
-    val ToolWindow = IconManager.getInstance().getIcon("/icons/toolWindow.svg", javaClass.getClassLoader())
-}
