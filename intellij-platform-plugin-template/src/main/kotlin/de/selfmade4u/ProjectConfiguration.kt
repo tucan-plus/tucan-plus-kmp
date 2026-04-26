5a335f74fdea3a57a556bfbbe8ebcc3949eaf5c7
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2026-04-26 22:23:44 +0200

    wip

--- intellij-platform-plugin-template/src/main/kotlin/de/selfmade4u/ProjectConfiguration.kt
+++ intellij-platform-plugin-template/src/main/kotlin/de/selfmade4u/ProjectConfiguration.kt
@@ -3,7 +3,9 @@ package de.selfmade4u
 import com.intellij.openapi.project.Project
 import org.jetbrains.kotlin.idea.base.util.projectScope
 import org.jetbrains.kotlin.idea.stubindex.KotlinAnnotationsIndex
+import org.jetbrains.kotlin.psi.KtNamedFunction
 import org.jetbrains.kotlin.psi.KtStringTemplateExpression
+import org.jetbrains.kotlin.psi.psiUtil.getParentOfType
 
 // https://github.com/JetBrains/intellij-community/blob/37b233a6b70fb606dad5f13eb001c63fb6b12cf4/plugins/kotlin/base/analysis/src/org/jetbrains/kotlin/idea/stubindex/IdeStubIndexService.java#L306
 // https://github.com/JetBrains/kotlin/blob/a66400f13ad48df4ed889ba98d94c0ece05d9acf/compiler/psi/psi-impl/src/org/jetbrains/kotlin/psi/stubs/elements/StubIndexService.kt#L12
@@ -21,6 +23,8 @@ class ProjectConfiguration {
             val valueArg = annotationEntry.valueArgumentList!!.arguments.first()
             val text = valueArg.getArgumentExpression() as KtStringTemplateExpression
             println("path ${text.entries.first().text}")
+            val ktNamedFunction = annotationEntry.getParentOfType<KtNamedFunction>(strict = true)
+            println("abc ${ktNamedFunction!!.text}")
         }
     }
 }
