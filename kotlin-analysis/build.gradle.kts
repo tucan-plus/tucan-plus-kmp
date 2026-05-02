7b33d01379e32f2461eab3385e37221d6bf7e538
Moritz Hedtke <Moritz.Hedtke@t-online.de>
2026-05-02 22:47:41 +0200

    wip

--- kotlin-analysis/build.gradle.kts
+++ kotlin-analysis/build.gradle.kts
@@ -24,13 +24,19 @@ dependencies {
     // https://github.com/JetBrains/amper/blob/13868b31d1442f7868bfd5f1fba68d43d41e8477/sources/extensibility/uses-aa.module-template.yaml#L15
     // https://youtrack.jetbrains.com/issue/KT-61419
     // https://youtrack.jetbrains.com/issue/KT-61639
+
+    implementation(libs.kotlin.compiler)
+
+    // maybe put this separately?
+    implementation(libs.kotlin.analysisApiStandalone) { isTransitive = false }
+
     implementation(libs.kotlin.analysisApi) { isTransitive = false }
     implementation(libs.kotlin.analysisApiK2) { isTransitive = false }
-
     implementation(libs.kotlin.analysisApiImplBase) { isTransitive = false }
     implementation(libs.kotlin.analysisApiPlatformInterface) { isTransitive = false }
     implementation(libs.kotlin.lowLevelApiFir) { isTransitive = false }
     implementation(libs.kotlin.symbolLightClasses) { isTransitive = false }
+
     testImplementation(kotlin("test"))
 }
 
--- kotlin-analysis/src/main/kotlin/Main.kt
+++ kotlin-analysis/src/main/kotlin/Main.kt
@@ -1,16 +1,22 @@
 package de.selfmade4u
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import java.nio.file.Paths
+import org.jetbrains.kotlin.analysis.api.standalone.buildStandaloneAnalysisAPISession
+import org.jetbrains.kotlin.analysis.project.structure.builder.buildKtLibraryModule
+import org.jetbrains.kotlin.analysis.project.structure.builder.buildKtSourceModule
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+fun main() {
+    val session = buildStandaloneAnalysisAPISession {
+        buildKtModuleProvider {
+            // Define your source roots and dependencies here
+            addModule(
+                buildKtSourceModule {
+                    addSourceRoot(Paths.get("src/main/kotlin"))
+                    moduleName = "my-module"
+                    // Add standard libraries so types like 'String' resolve
+                    addRegularDependency(buildKtLibraryModule { })
+                }
+            )
+        }
     }
 }
